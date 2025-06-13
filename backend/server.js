01fec3fce3bfffb8d5025f5eb8ca7e36205f6713
Anchal Jain <jainanchal000@gmail.com>
2025-06-13 23:50:53 +0530

    Updated some mongo code

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 const urlSchema = new mongoose.Schema({
   originalUrl: String,
   shortUrl: String,
